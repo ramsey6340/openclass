a86b2430ccf7796c3bed9d662c6b3290a56f1e12
ramsey6340 <eniac5project@gmail.com>
2022-10-17 02:58:08 +0200

    creation d'une classe avec une methode static pour affichier les page sheets sans faire de répetition de code

---
+++ lib/view/composants/show_setting_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '../constante.dart';
+
+/// Cette classe contient la methode qui permet de retourner la page de reglage
+/// des differents page de chat
+class ShowSettingPage
+{
+
+  static void showSettingPage(BuildContext context, Widget setting_page)
+  {
+    showModalBottomSheet(
+        backgroundColor: kColorDrawer,
+        shape: kSettingChatPageShape,
+        isScrollControlled: true,
+        context: context,
+        builder: (context){
+          return setting_page;
+        }
+    );
+  }
+}
